03714c71b4f7ea241d66b50bc6120ae64ad55b05
Guilherme <guitue@gmail.com>
2023-08-03 11:01:01 -0300

    finish chapter 7

---
+++ cap07/ex01/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Qual é a diferença entra método e função?")
+
+	fmt.Println("Métodos são acessados por meio das structs, isto é, uma função acessada direto do objeto.")
+
+	fmt.Println("Já as funções, não são atreladas à objetos.")
+}
---
+++ cap07/ex02/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Por que usariamos um campo anônimo incluido em vez de utilizar um campo nomeado? ")
+
+	fmt.Println("Pois o campo anônimo nos da a propriedade da struct `ser` daquele tipo também e não `ter` o tipo.")
+
+	fmt.Println("Com isso, a struct nova também tem acesso aos metodos do campo anônimo.")
+
+}
